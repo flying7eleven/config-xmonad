2193c0fc6a192c60fd2199f5f15705f56da69c4f
Tim Hütz <tim@huetz.biz>
2015-05-27 15:53:59 +0200

    Add a max. and min. brightness check

--- changeBrightness.sh
+++ changeBrightness.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-cur_bri=$(/usr/bin/cat /sys/class/backlight/gmux_backlight/brightness)
+currentBrightness=$(/usr/bin/cat /sys/class/backlight/gmux_backlight/brightness)
+maximumBrightness=$(/usr/bin/cat /sys/class/backlight/gmux_backlight/max_brightness)
 changeAmount=5000
 if [ $1 == "up" ] ; then
-	bri=$(($cur_bri+$changeAmount))
+	bri=$(($currentBrightness+$changeAmount))
+	if [ $bri -gt $maximumBrightness ] ; then
+		bri=$maximumBrightness
+	fi
 	`echo $bri > /sys/class/backlight/gmux_backlight/brightness`
 fi
 if [ $1 == "down" ] ; then
-	bri=$(($cur_bri-$changeAmount))
+	bri=$(($currentBrightness-$changeAmount))
+	if [ $bri -lt 0 ] ; then
+		bri=0
+	fi
 	`echo $bri > /sys/class/backlight/gmux_backlight/brightness`
 fi
