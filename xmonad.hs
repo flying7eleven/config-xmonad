a8f1636c490c40f12679c79cdaeeabade64efa49
Tim Hütz <tim@huetz.biz>
2015-06-02 06:59:17 +0200

    Add a key binding for toggling the layout

--- xmonad.hs
+++ xmonad.hs
@@ -4,6 +4,7 @@ import XMonad.Util.Run( spawnPipe )
 import XMonad.Hooks.SetWMName
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
+import XMonad.Layout.ToggleLayouts
 import System.IO ( hPutStrLn )
 
 -- define the mod mask as a variable to be used in the keybindings and the basic settings as well 
@@ -12,9 +13,10 @@ myModMask = mod4Mask
 -- define the list of my custom key bindings to add to xmonad
 myKeyBindings = 
 	[
-		( ( myModMask,  xK_r ), spawn "dmenu_run" ),
-		( ( myModMask,  xK_l ), spawn "xlock -mode forest" ),
-		( ( myModMask,  xK_Return ), spawn "terminator" ),
+		( ( myModMask, xK_r ), spawn "dmenu_run" ),
+		( ( myModMask, xK_l ), spawn "xlock -mode forest" ),
+		( ( myModMask, xK_Return ), spawn "terminator" ),
+		( ( myModMask, xK_f ), sendMessage ToggleLayout ),
 		( ( 0, 0x1008ff03 ), spawn "terminator" ), -- brightness down
 		( ( 0, 0x1008ff02 ), spawn "terminator" ), -- brightness up
 		( ( 0, 0x1008ff4a ), spawn "terminator" ) -- toggle the tray and the xmobar
